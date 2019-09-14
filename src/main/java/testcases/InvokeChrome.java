5073a50843f194f2617b788e6c6a61361ec66ec2
ymammulavenkata <ymammulavenkata@USHYDYMAMMULAV1.us.deloitte.com>
2019-09-14 12:53:56 +0530

    test on grid

--- src/main/java/testcases/InvokeChrome.java
+++ src/main/java/testcases/InvokeChrome.java
@@ -1,15 +1,28 @@
 package testcases;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+
+import com.gargoylesoftware.htmlunit.javascript.configuration.BrowserName;
 
 public class InvokeChrome {
 
 	@Test(groups = {"tuesday"})
-	public void chromeTest() {
+	public void chromeTest() throws MalformedURLException {
+		RemoteWebDriver driver = null;
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName("chrome");
+		dc.setPlatform(Platform.WIN10);
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+//		driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL("http://localhost.4444/wd/hub"), dc);
 		driver.get("https://www.moneycontrol.com");
 		System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.findElement(By.id("getAppModel")).getAttribute("class"));
