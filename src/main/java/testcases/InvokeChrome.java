a5ae04ad0476404bb87aff5e002f3ff3fde91248
ymammulavenkata <ymammulavenkata@USHYDYMAMMULAV1.us.deloitte.com>
2019-09-14 12:41:17 +0530

    test changes

--- src/main/java/testcases/InvokeChrome.java
+++ src/main/java/testcases/InvokeChrome.java
@@ -1,5 +1,6 @@
 package testcases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -7,9 +8,11 @@ public class InvokeChrome {
 
 	@Test(groups = {"tuesday"})
 	public void chromeTest() {
-		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.moneycontrol.com");
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.findElement(By.id("getAppModel")).getAttribute("class"));
+		driver.close();
 	}
 }
